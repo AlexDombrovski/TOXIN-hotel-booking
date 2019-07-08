172f7b2f423fd572dc21b7957c08b0e68de7b344
Alex Dombrovski <ad19702004@mail.ru>
2019-07-08 17:12:29 +0700

    Завершение checkboxes main-page

--- build/js/checkboxes-main-page.js
+++ build/js/checkboxes-main-page.js
@@ -1,3 +1,4 @@
+// Блок "Checkbox Buttons"
 let labels = document.querySelectorAll('.ch-boxes'),
     checkBoxes = document.querySelectorAll('.checkboxes'),
     avail = document.querySelectorAll('.avail'),
@@ -15,7 +16,22 @@ for (let i = 0; i < avail.length; i++) {
   });
 }
 
+// Блок "дополнительные удобства":
 
+let moreConveniencesArrow = document.querySelector('.more-conveniences__arrow'),
+    moreConveniencesList = document.querySelector('.more-conveniences__list'),
+    conveniencesLabel = document.querySelectorAll('.conveniences__label'),
+    moreConveniencesBox = document.querySelectorAll('.more-conveniences__box');
 
+// открытие блока
+    moreConveniencesArrow.addEventListener('click', function() {
+      moreConveniencesArrow.classList.toggle('more-conveniences__arrow-down');
+      moreConveniencesList.classList.toggle('more-conveniences__list-visible')
+    });
 
-  
+// кликаем чекбоксы
+    for (let i = 0; i < conveniencesLabel.length; i++) {
+      conveniencesLabel[i].addEventListener('click', function() {
+        moreConveniencesBox[i].classList.toggle('more-conveniences__box-down')
+      });
+    }
