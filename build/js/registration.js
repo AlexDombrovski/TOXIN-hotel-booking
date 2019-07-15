a15b8e7902c08302b1cd9c2a12b7c78f6fa2c16d
Alex Dombrovski <ad19702004@mail.ru>
2019-07-15 18:44:53 +0700

    Testing JS on result-page

--- build/js/registration.js
+++ build/js/registration.js
@@ -4,7 +4,13 @@ let nameInput = document.querySelector('.name'),
     genderInput = document.querySelectorAll('.genderinput'),
     buttonToggle = document.querySelector('.toggle'),
     buttonToggleOff = document.querySelector('.toggle-of'),
-    buttonPay = document.querySelector('.new-account__button');
+    buttonPay = document.querySelector('.new-account__button'),
+    dateBirth = document.querySelector('#booking-dates'),
+    emailInput = document.querySelector('#email'),
+    passInput = document.querySelector('#pass'),
+    newAccountBox = document.querySelector('.new-account-box'),
+    newAcc = document.querySelector('.new-acc');
+
 
   
 
@@ -35,9 +41,47 @@ buttonToggle.addEventListener('click', function (evt) {
 // result (click on buttonPay)
 buttonPay.addEventListener('click', function (evt) {
   evt.preventDefault();
-  let nameValue = nameInput.value,
-      surnameValue = surnameInput.value;
-  if (nameInput || surnameInput) {
-    console.log(nameValue, surnameValue);
+      
+  if (nameInput || surnameInput || dateBirth || emailInput || passInput) {
+    console.log(nameInput.value, surnameInput.value);
+    console.log(dateBirth.value);
+    console.log(emailInput.value);
+    console.log(passInput.value);
+    newAccountBox.classList.add('new-account-box-off');
+
+    var divMessage = document.createElement('div');
+        divMessage.classList.add('message-result');
+        newAcc.appendChild(divMessage);
+        divMessage.classList.toggle('message-result-on');
+
+    var messageTitle = document.createElement('div');
+        messageTitle.classList.add('message-result__title');
+        messageTitle.textContent = 'Регистрационные данные';
+        divMessage.appendChild(messageTitle);
+        
+    var name = document.createElement('div');
+        name.classList.add('message-result__name');
+        name.textContent = 'Имя: ' + nameInput.value;
+        divMessage.appendChild(name);
+
+    var surName = document.createElement('div');
+        surName.classList.add('message-result__name');
+        surName.textContent = 'Фамилия: ' + surnameInput.value;
+        divMessage.appendChild(surName);
+
+    var birthDay = document.createElement('div');
+        birthDay.classList.add('message-result__name');
+        birthDay.textContent = 'Дата рождения: date picker under repair :)';
+        divMessage.appendChild(birthDay);
+
+    var email = document.createElement('div');
+        email.classList.add('message-result__name');
+        email.textContent = 'Email: ' + emailInput.value;
+        divMessage.appendChild(email);
+
+    var pass = document.createElement('div');
+        pass.classList.add('message-result__name');
+        pass.textContent = 'Пароль: ' + passInput.value;
+        divMessage.appendChild(pass);
   }
 });
